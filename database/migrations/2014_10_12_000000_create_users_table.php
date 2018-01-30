0c6565b723b451ccf95e478e1538d5136b28d720
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-01-30 13:47:11 +0000

    User migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -18,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('verify_token')->default(User::createVerifyToken());
+            $table->string('state')->default(Config::get('customConstants.user.state.is_not_verified'));
             $table->rememberToken();
             $table->timestamps();
         });
