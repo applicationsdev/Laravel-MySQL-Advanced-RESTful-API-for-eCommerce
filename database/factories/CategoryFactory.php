7bb3015396651993d4007d15403f5d0959bc0572
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-02-04 15:07:15 +0000

    CategoryFactory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,9 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Category;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Category::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word,
+        'image_thumbnail' => $faker->imageUrl($width = 500, $height = 500),
+        'description_short' => $faker->text($maxNbChars = 90),
+        
+        'status' => $faker->randomElement([
+            Config::get('customConstants.category.status.is_not_active'),
+            Config::get('customConstants.category.status.is_active'),
+        ]),
     ];
 });
