50dc2b132d7dea5b8544f56e5bdb036c1c65918d
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-03-31 15:35:23 +0000

    DatabaseSeeder upd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Database\Seeder;
 
+// import Models
+use App\User;
+use App\Category;
+use App\Item;
+use App\Order;
+use App\OrderItem;
+
+// import DB Facade
+use Illuminate\Support\Facades\DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +21,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // In case we create multiple Seeders, we can use the following syntax example
         // $this->call(UsersTableSeeder::class);
+        
+        // Before every DB 'seeding', it's a good practice
+        // to deactivate FOREIGN_KEY_CHECKS
+        // and delete old data from DB tables
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        User::truncate();
+        Category::truncate();
+        Item::truncate();
+        Order::truncate();
+        OrderItem::truncate();
+        
+        // Define 'How Many' fake data will be created for testing
+        $howManyUsers = 30;
+        $howManyCategories = 12;
+        $howManyItems = 20;
+        $howManyOrders = 10;
+        $howManyOrderItems = 14; // forcing some Orders to have more than one OrderItems
+        
+        // Generate fake data for testing
+        // Note: to simplify testing,
+        // but at the same time to simulate RDBMS behavior (foreign keys)
+        // I create Order before OrderItem and then I update and correct
+        // each Order's 'value' field.
+        // It's a good practice to consider Entities Relationships
+        // to decide which Factory should run first, second, etc..
+        factory(User::class, $howManyUsers)->create();
+        factory(Category::class, $howManyCategories)->create();
+        factory(Item::class, $howManyItems)->create();
+        factory(Order::class, $howManyOrders)->create();
+        
+        factory(OrderItem::class, $howManyOrderItems)->create()->each(
+            function($orderitem){
+                Order::all()
+                    ->where('id', '=', $orderitem->order_id)
+                    ->increment('value', $orderitem->value);
+            }
+        );
     }
 }
